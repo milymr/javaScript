57f200f2069c64354adda064a8535d36a6baeadc
milymr <eemilymariaribeiro@gmail.com>
2025-01-29 08:19:20 -0300

    Initial commit

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,29 @@
+console.log("Olá, Mundo!")
+
+let nome = "Juliana"//vamos preferir esse
+var nomes = "Juliana" 
+
+
+//CTRL + ; -> comentarios
+console.log(nome)
+
+let idade = 24 
+console.log("Minha idade é " , idade)
+//concatenação  = juntar texto e valor 
+
+
+//operações matemáticas 
+let a = 3
+let b = 2
+
+let soma = a + b 
+let sub = a - b 
+let mult = a * b 
+let div = a / b 
+let resto = a % b //o que sobra da divisão 
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
