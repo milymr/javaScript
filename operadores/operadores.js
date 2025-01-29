08b2742ec2628144f5ee116bbd7bc2c25e938e2d
milymr <eemilymariaribeiro@gmail.com>
2025-01-29 11:24:13 -0300

    .

---
+++ operadores/operadores.js
@@ -0,0 +1,72 @@
+//qu1
+a = 10
+b = 20
+
+soma = a + b
+
+console.log("A soma das variáveis é:", soma)
+
+
+a = 12
+b = 14
+
+subtracao = a - b 
+
+console.log("a subtração é:", subtracao)
+
+a = 65
+b = 5 
+
+divisao = a / b 
+console.log("A divisão é ", divisao)
+
+a = 90 
+b = 70
+
+resto = a % b
+console.log("O resto é", resto)
+
+//qu2
+
+l = 65 
+h = 45
+
+multi = l * h 
+
+console.log("A area é", multi)
+
+//qu3
+
+a = 9
+b = 3
+
+multi = a * 2
+divisao = a / 2 
+
+console.log("o dobro de 9 é", multi)
+console.log ("a metade do numero 9 é", divisao )
+
+multi = b * 2
+divisao = b / 2 
+
+console.log("o dobro de 3 é", multi)
+console.log ("a metade do numero 3 é", divisao )
+
+//qu4
+
+a1 = 7
+a2 = 8
+a3 = 9 
+
+media = (a1 + a2 + a3) / 3 
+console.log ("A média é", media)
+ 
+//qu5
+c = 23
+
+f = (c * 9/5) + 32 
+console.log("o calculo é", f )
+
+
+
+
