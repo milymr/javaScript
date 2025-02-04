f5d2ea105a239ce28bb36775b26e877790138786
milymr <eemilymariaribeiro@gmail.com>
2025-02-04 10:13:49 -0300

    lista ex03

---
+++ estrutura condicional/encadeada.js
@@ -0,0 +1,9 @@
+// Número positivo, negativo ou zero
+let numero = prompt ("Escolha um número:")
+if(numero > 0) {
+    console.log("positivo")
+} else if (numero < 0 ) {
+   console.log("Negativo")
+} else {
+    console.log("Zero")
+}
