1356c57f31b4c2af23231f06fe1b3c47541f5bf5
milymr <eemilymariaribeiro@gmail.com>
2025-02-25 08:47:01 -0300

    jogo

---
+++ funçoes/anonima.js
@@ -0,0 +1,15 @@
+//função anonima
+let dobro = function(numero){
+    let resultado = numero * 2
+    return resultado
+}
+console.log(dobro(5))
+
+//arrow function 
+let vezesDois = (numero) => {
+    let resultado = numero * 2
+    return resultado
+}
+console.log(vezesDois(8))
+
+//questao 01                                                                     
